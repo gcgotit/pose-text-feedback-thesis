9045e4c669bed1b5b78cca0e88876cf386d223cd
Giovanni Conter <workgiovannic@gmail.com>
2025-12-22 12:37:29 +0100

    a

--- models/dual_encoder/train_dual_encoder.py
+++ models/dual_encoder/train_dual_encoder.py
@@ -154,7 +154,7 @@ def train_dual_encoder(temperature, patience=5, device="cuda"):
 
     # Optimizer
     optimizer = optim.AdamW(params, lr=1e-4)
-    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=100)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
 
     '''
     # Istanza GradNorm
